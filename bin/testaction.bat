bfc7f602fab936206b8a940f181cda93756ec9a5
oktane <oktane@gmail.com>
2010-09-24 06:34:59 -0500

    Documentation

--- bin/testaction.bat
+++ bin/testaction.bat
@@ -1,4 +1,6 @@
 @echo off
+echo This demo shows that rssRunCmd has run an application, this batch file.
+echo Below are the parameters that are passed to the application. (See README)
 echo param1:
 @echo %1
 echo param2:
@@ -11,8 +13,6 @@ echo param5:
 @echo %5
 echo param6:
 @echo %6
-echo param7:
-@echo %7
 
 echo working directory:
 cd
